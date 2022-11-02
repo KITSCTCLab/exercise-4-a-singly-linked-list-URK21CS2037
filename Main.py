517d4bacca2d40836771089a2e277fc5e78a4cf0
URK21CS2037 <109648094+URK21CS2037@users.noreply.github.com>
2022-11-02 14:07:57 +0530

    update final_j

--- Main.py
+++ Main.py
@@ -8,13 +8,25 @@ class Node:
         -> data - stored associated data
         -> next - link to next node
     """
-    def __init__(self,data=None,next=None):
+
+    def __init__(self, data=None, next=None):
+        """
+        Initialises the Node with given attributes
+        """
         self.data = data
         self.next = next
 
 
 class LinkedList:
-    
+    """
+    This class implements LinkedList using Node objects
+    Methods:
+        -> insert_at_end - inserts node with data at the end of the list
+        -> status - displays all elements of the lisT
+    Attributes
+        -> self.head - contains first node of LinkedList, None if list empty
+    """
+
     def __init__(self):
         """
         Initialize the head
@@ -46,15 +58,31 @@ class LinkedList:
             current = current.next
         print(elements)
 
+
 class Solution:
-    def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[LinkedList]:
+    """
+    Class implementing functions to add numbers in a LinkedListT
+    
+    """
+
+    def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[
+        LinkedList]:
+        """
+        :param first_list: Linkedlist with non-negative integers
+        :param second_list: Linkedlist with non-negative integers
+        :return: returns the sum as a linked list
+        """
         result = self.get_num(first_list) + self.get_num(second_list)
         sum_list = LinkedList()
         for digit in list(map(int, str(result)[::-1])):
             sum_list.insert_at_end(digit)
         return sum_list
-        
-     def get_num(self,l:Optional[LinkedList])->int:
+
+    def get_num(self, l: Optional[LinkedList]) -> int:
+        """
+        :param l: LinkedList with non-negative integers
+        :return: returns digits of the list as a single integer
+        """
         curr = l.head
         if curr is None:
             return 0
@@ -63,12 +91,12 @@ class Solution:
             num = str(curr.data) + num
             curr = curr.next
         return int(num)
-        
 
-# Do not edit the following code      
+
+# Do not edit the following code
 # Create an instance for LinkedList
 first_list = LinkedList()
-# Create an another instance for LinkedList
+# Create an another instance for LinkedListT
 second_list = LinkedList()
 # Read data for first list
 data_for_first_list = list(map(int, input().strip().split(" ")))
